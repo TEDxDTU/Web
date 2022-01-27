b9afc2b2003efd050fb117b7743baa1634568dd7
Shivam311201 <gargshivam311201@gmail.com>
2022-01-27 23:37:23 +0530

    added PrimaryTheme

--- components/Home/Landing.js
+++ components/Home/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavBar from '../Universal/NavBar';
 import Page from '../Universal/Page';
+import PrimaryTheme from './PrimaryTheme';
 
 export default function Landing() {
   return (<Page>
-    <div>Landing Page</div>
+    <div>
+      <PrimaryTheme/>
+      {/* <div>jgfdhfsdhf</div> */}
+    </div>
   </Page>);
 };

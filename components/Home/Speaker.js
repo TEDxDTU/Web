4e2629b76f251afefdf0a4f5f99660284ee0d047
shivang <shivangmathur101@gmail.com>
2022-02-02 20:08:48 +0530

    Added responsive colarge and about-us components

--- components/Home/Speaker.js
+++ components/Home/Speaker.js
@@ -1,98 +1,60 @@
+import Box from "./Box";
+import Colarge from "./colarge";
+
 const Speaker = () => {
   return (
     <>
-      <div className="bg-black">
-        <main className="grid lg:grid-cols-2 xl:grid-cols-11 2xl:gap-12  xl:px-20 2xl:px-24 mx-14 py-8">
-          <article className="bg-gray-200 lg:col-span-1 xl:col-span-6 2xl:col-span-7"></article>
+      <div className="border-t-4 border-red-700 bg-black">
+        <main className="grid lg:grid-cols-2 xl:grid-cols-11 2xl:gap-12  xl:px-2 2xl:px-24 mx-14 py-8">
+          <article className="bg-black lg:col-span-1 xl:col-span-6 2xl:col-span-7 max-h-fit mb-20">
+            <span className="flex flex-col mt-5 2xl:mt-10 ">
+              <div className="text-white text-6xl 2xl:text-8xl ml-20 2xl:ml-28 flex gap-8">
+                About
+                <img className=" mt-1" src="/Images/Logo-White-Text.svg" />
+              </div>
 
-          <article className="bg-black sm:ml-9 sm:px-5 md:px-16 md:mx-9 lg:mx-0 lg:px-0 lg:col-span-1 xl:col-span-5 2xl:col-span-4 2xl:px-6">
-            <span className="grid grid-cols-3 justify-items-center items-center">
-              <div className="   h-[7.688rem] w-[7.5rem]">
-                <img
-                  className="w-full"
-                  src="About-Us_img\R1C1 Daggubati-Rana.png"
-                />
+              <div className="bg-box text-xl lg:text-base xl:text-xl text-white py-5 px-10 rounded-xl m-10 2xl:mb-20">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                reprehenderit totam iste vel dignissimos, laudantium eum
+                eligendi temporibus ea in quidem explicabo placeat aperiam eos
+                doloribus a ad unde consequatur nam. Assumenda porro ullam
+                consequuntur debitis natus nemo, tenetur accusamus quibusdam
+                praesentium. Rerum distinctio debitis nisi nemo esse
+                consequuntur provident vel quis modi placeat aliquam, assumenda,
+                omnis obcaecati voluptas officiis ducimus quam accusamus iste
               </div>
-              <div className="  mt-2 ml-6 h-[8.688rem] w-[12.125rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R1C2 Ishita-Katyal.png"
-                />
+
+              <div className="text-white text-6xl 2xl:text-8xl ml-20 2xl:ml-28 flex">
+                What is<span className="text-logo_red"> TED</span>
               </div>
-              <div className="  ml-6 -mt-8 h-[5.688rem] w-[4.938rem]">
-                <img className="w-full h-full" src="About-Us_img\R1C3.png" />
+
+              <div className="bg-box text-xl lg:text-base xl:text-xl text-white py-5 px-10 rounded-xl m-10 mb-20">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                reprehenderit totam iste vel dignissimos, laudantium eum
+                eligendi temporibus ea in quidem explicabo placeat aperiam eos
+                doloribus a ad unde consequatur nam. Assumenda porro ullam
+                consequuntur debitis natus nemo, tenetur accusamus quibusdam
+                praesentium. Rerum distinctio debitis nisi nemo esse
+                consequuntur provident vel quis modi placeat aliquam, assumenda,
+                omnis obcaecati voluptas officiis ducimus quam accusamus iste
               </div>
-              <div className="  -mt-16 h-[10.75rem] w-[8.688rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R2 C1 EmilyEsfahaniSmith_2017-embed 1.png"
-                />
-              </div>
-              <div className="  mt-5 ml-10 h-[16rem] w-[10.625rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R2 C2 ChristinePorath_2018X-embed.png"
-                />
-              </div>
-              <div className="  ml-8 -mt-32 h-[12.5rem] w-[6.1rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R2 C3 maxresdefault (3).png"
-                />
-              </div>
-              <div className="  -mt-32 h-[12.75rem] w-[9.688rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R3 C1 VikasKhanna_2017I-1350x675.png"
-                />
-              </div>
-              <div className="  mt-4 ml-7 h-[12.9rem] w-[8.625rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R3 C2 TEDxYYC2019_403_sm-1024x684.png"
-                />
-              </div>
-              <div className="  ml-6 -mt-40 h-[13.813rem] w-[7rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R3 C3 UCIMG-502-2048x1150.png"
-                />
-              </div>
-              <div className="  -mt-28 h-[9.18rem] w-[9.5rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R4 C1 maxresdefault (4).png"
-                />
-              </div>
-              <div className="  -mt-10 ml-8 h-[4.75rem] w-[8.5rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R4 C2 maxresdefault (5).png"
-                />
-              </div>
-              <div className="  -mt-14 h-[12.563rem] w-[7rem]">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R4 C3 RituKaridhal_2017I-stageshot.png"
-                />
-              </div>
-              <div className="  -mt-8 h-[12.625rem] w-[20.313rem] col-span-2">
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R5 C1 C2 21753471_509849532681465_1180612420614830843_o.png"
-                />
-              </div>
-              <div
-                className="mt-6
-                h-[9.18rem] w-[7.75rem]"
-              >
-                <img
-                  className="w-full h-full"
-                  src="About-Us_img\R5 C3 DeepaNarayan_2019I-embed.png"
-                />
-              </div>
+
+              <footer className="flex sm:justify-center 2xl:justify-start 2xl:flex-none ">
+                <div className="bg-box_red min-h-[9rem] max-w-[16rem] md:w-[20rem] -mt-14 rounded-3xl mx-10 grid grid-cols-2 justify-items-center place-content-center text-xl text-white">
+                  <div>
+                    Ted talk <br />
+                    with <br />
+                    Sarthak
+                  </div>
+                  <div className="flex flex-col gap-4 items-end">
+                    <div className="mr-2">Live</div>
+                    <div className="bg-logo_red h-fit w-fit p-2">Join Now</div>
+                  </div>
+                </div>
+              </footer>
             </span>
           </article>
+          <Colarge />
         </main>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        box: "#232323",
+        logo_red: "#FF2B06",
+        box_red: "#73150F",
+      },
+    },
   },
   plugins: [],
 };

5eef54cb9094efbf5d58faa7f033dff633469b46
shivang <shivangmathur101@gmail.com>
2022-02-03 16:48:58 +0530

    Completed Collage

--- components/Home/Speaker.js
+++ components/Home/Speaker.js
@@ -1,5 +1,5 @@
 import AboutContent from "./AboutPage_components/AboutContent";
-import Colarge from "./AboutPage_components/Colarge";
+import Collage from "./AboutPage_components/Collage";
 
 const Speaker = () => {
   return (
@@ -7,7 +7,7 @@ const Speaker = () => {
       <div className="border-t-4 border-red-700 bg-black">
         <main className="grid lg:grid-cols-2 xl:grid-cols-11 2xl:gap-12  xl:px-2 2xl:px-24 mx-14 py-8">
           <AboutContent />
-          <Colarge />
+          <Collage />
         </main>
       </div>
     </>

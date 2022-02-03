3cd446d14e2cbf1f3e952165c4f5f949edab8dab
shivang <shivangmathur101@gmail.com>
2022-02-03 18:52:19 +0530

    Refactored img tags in collage

--- components/About/AboutContent.js
+++ components/About/AboutContent.js
@@ -6,7 +6,10 @@ const AboutContent = () => {
       <span className="flex flex-col mt-5 2xl:mt-10 ">
         <div className="text-white text-6xl 2xl:text-8xl ml-20 2xl:ml-28 flex gap-8">
           About
-          <img className=" mt-1" src="/Images/Logo-White-Text.svg" />
+          <img
+            className=" mt-1"
+            src="/LandingPage-Assets/Logo-White-Text.svg"
+          />
         </div>
 
         <div className="bg-box text-xl lg:text-base xl:text-xl text-white py-5 px-10 rounded-xl m-10 2xl:mb-20">
@@ -22,7 +25,7 @@ const AboutContent = () => {
         </div>
 
         <footer className="flex sm:justify-center 2xl:justify-start 2xl:flex-none ">
-          <div className="bg-box_red min-h-[9rem] max-w-[16rem] md:w-[20rem] -mt-14 rounded-3xl mx-10 grid grid-cols-2 justify-items-center place-content-center text-xl text-white">
+          <div className="bg-box_red min-h-[9rem] max-w-[16rem] md:w-[20rem] -mt-6 rounded-3xl mx-10 grid grid-cols-2 justify-items-center place-content-center text-xl text-white">
             <div>
               Ted talk <br />
               with <br />

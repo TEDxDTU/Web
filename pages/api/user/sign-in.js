784fc62f0b0497b36779476b5ba85916ecb4082c
Saarthak <zsaarthakz@gmail.com>
2022-02-25 20:56:46 +0530

    Added Sign-Up API End Point

---
+++ pages/api/user/sign-in.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+
+  };
+};  
---
+++ pages/api/user/sign-up.js
@@ -0,0 +1,68 @@
+const admin = require("firebase-admin");
+const creds = require("../../../tedx-dtu-firebase-adminsdk-creds.json");
+const User = require("../../../backend/schemas/user");
+const mongooseConnect = require("../../../backend/mongooseConnect");
+
+export default async function handler(req, res) {
+
+  if (admin.apps.length === 0) admin.initializeApp({
+    credential: admin.credential.cert(creds)
+  });
+
+  try {
+    if (req.method === "POST") {
+      const { name, email, password, university, imageURL } = req.body;
+
+      const auth = admin.auth();
+      let existingUser;
+
+      try {
+        existingUser = await auth.getUserByEmail(email);
+      } catch (err) { };
+
+      if (existingUser) {
+        res.json({
+          msg: "User with given email already exists"
+        });
+        return;
+      }
+
+
+      const newDBUser = User({
+        name,
+        email,
+        university,
+        imageURL,
+      });
+
+      try {
+        await newDBUser.save();
+
+        const newFirebaseUser = await auth.createUser({
+          email: email,
+          // emailVerified: false,
+          password: password,
+          displayName: name,
+          disabled: false,
+          // photoURL: imageURL
+        });
+
+        newDBUser.firebaseID = newFirebaseUser.uid;
+
+        await newDBUser.save();
+
+        return res.json(newDBUser);
+
+      } catch (err) {
+        res.status(500).json({
+          msg: err.toString()
+        });
+      };
+
+    };
+
+  } catch (err) {
+    console.log(err);
+  }
+
+};  

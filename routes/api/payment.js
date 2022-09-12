c938be581990d4220e3349b421bf5e48f888a6a8
Shivam311201 <gargshivam311201@gmail.com>
2022-09-12 21:54:56 +0530

    Razorpay Fixed

--- routes/api/payment.js
+++ routes/api/payment.js
@@ -1,10 +1,11 @@
-"use strict";
+  "use strict";
 
 const express = require("express");
 const router = express.Router();
 const shortid = require('shortid')
 const User = require("../../schemas/user");
 const admin = require("firebase-admin");
+const crypto = require('crypto');
 const razorpayLib = require("razorpay");
 const Razorpay = new razorpayLib({
   key_id: 'rzp_test_P5GUEZ3SzH045X',
@@ -44,29 +45,24 @@ router.post("/generate-order", async (req, res) => {
 
 router.post("/verify-order-signature", (req, res) => {
 
-  console.log(req.body);
+  const SECRET='12345678';  
+  const hash = crypto.createHmac('SHA256', SECRET).update(JSON.stringify(req.body)).digest('hex');
 
-  res.json({status:'OK'});
-
-  // const { paymentID, serverOrderID, paymentSignature } = req.body;
-  // const hash = crypto.createHmac('SHA256', devConfig.RAZORPAY_KEY_SECRET).update(serverOrderID + "|" + paymentID).digest('hex');
-
-  // if (paymentSignature !== hash) {
-  //   res.json({
-  //     success: false,
-  //     msg: "Payment can not be verified and hence declined",
-  //     redirectURL: "",
-  //   });
-  //   return;
-  // };
+  if (req.headers['x-razorpay-signature'] !== hash) {
+    res.json({
+      success: false,
+      msg: "Payment can not be verified and hence declined",
+      redirectURL: "",
+    });
+    return;
+  };
 
   // // Add the Cart Items to Orders using serverOrderID and generate an invoice
-
-  // res.json({
-  //   success: true,
-  //   msg: "Payment verified and accepted",
-  //   redirectURL: "",
-  // }).status(200);
+  res.json({
+    success: true,
+    msg: "Payment verified and accepted",
+    redirectURL: "",
+  }).status(200);
 
 });
 

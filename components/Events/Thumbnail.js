f5927617eb878be16d61f8dddccfb70b4153fff2
HARSH BATRA <harshbat22@gmail.com>
2022-09-17 20:24:12 +0530

    modified register and live now buttons, view more on events page, etc.

--- components/Events/Thumbnail.js
+++ components/Events/Thumbnail.js
@@ -6,30 +6,40 @@ const Thumbnail = ({ event, eventType, setDisplay, setEventInfo }) => {
   const { title, imageUrl, details, dateTime } = event;
   const router = useRouter();
 
-  const viewMoreHandler = () =>{
-    if(eventType === "live"){
+  const viewMoreHandler = () => {
+    if (eventType === "live") {
       router.push(`/live-event`);
       return;
     }
     console.log(event);
     router.push(`/events/${event._id}`);
-  }
+  };
 
   return (
     <div className="shadow-md h-36 w-60 mx-8 mb-16 pb-10 border-2 border-[#737373]">
       <div className="relative">
         <img src={imageUrl} className="h-36 w-60" />
         <div className="absolute top-2 left-2">{title}</div>
-        {(eventType === 'upcoming') && <div className="absolute right-0 bottom-2" onClick={() => {
-          setDisplay(true);
-          setEventInfo(event);
-        }}>
-          <div className="rounded-2xl cursor-pointer duration-200 delay-75 transition hover:bg-red-600 hover:text-white bg-white text-black p-1 mr-2 font-semibold">Book Now</div>
-        </div>}
+        {eventType === "upcoming" && (
+          <div
+            className="absolute right-0 bottom-2"
+            onClick={() => {
+              setDisplay(true);
+              setEventInfo(event);
+            }}
+          >
+            <div className="rounded-2xl cursor-pointer duration-200 delay-75 transition hover:bg-red-600 hover:text-white bg-white text-black py-1 px-3 mr-2 font-semibold">
+              Book Now
+            </div>
+          </div>
+        )}
       </div>
-      <div className="bg-[#303030] p-1 pl-2 outline outline-2 outline-[#737373]">{details.substring(0, 50)}
-        {details.length > 50 ? "...." : ""}
-        <button onClick={viewMoreHandler}>View More</button>
+      <div className="bg-[#303030] p-1 pl-2 outline outline-2 outline-[#737373]">
+        {details.substring(0, 50)}
+        {details.length > 50 ? "..." : ""}
+        <button className="text-red-600 ml-2" onClick={viewMoreHandler}>
+          View More
+        </button>
       </div>
     </div>
   );
--- components/Universal/NavBar.js
+++ components/Universal/NavBar.js
@@ -7,8 +7,7 @@ export default function NavBar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [form, setForm] = useContext(FormContext);
 
-  useEffect(function () {
-  }, []);
+  useEffect(function () {}, []);
   useEffect(
     () =>
       (async () => {
@@ -68,7 +67,7 @@ export default function NavBar() {
           </div>
         </Link>
 
-        <Link href="/theme">
+        {/* <Link href="/theme">
           <div
             className="
         hover-underline-animation
@@ -80,7 +79,7 @@ export default function NavBar() {
           >
             Theme
           </div>
-        </Link>
+        </Link> */}
 
         <Link href="/events">
           <div
@@ -95,8 +94,6 @@ export default function NavBar() {
             Events
           </div>
         </Link>
-
-
 
         <Link href="/partners">
           <div
@@ -126,28 +123,30 @@ export default function NavBar() {
           </div>
         </Link>
 
-        {form && <Link href="/dashboard">
-          <div
-            className="
+        {form && (
+          <Link href="/dashboard">
+            <div
+              className="
         hover-underline-animation
         pb-2
         hover:text-red-600
         hover:border-red-600
         cursor-pointer
         "
-          >
-            Dashboard
-          </div>
-        </Link>}
+            >
+              Dashboard
+            </div>
+          </Link>
+        )}
 
         <Link href={form ? "/" : "/register"}>
           <button
             className="
         bg-red-600
         col-span-2
-        w-3/4
-        h-16
-        text-lg
+        w-3/5
+        h-12
+        text-lg hover:border-red-600 hover:border-2 hover:bg-white hover:text-red-600 
         "
             onClick={() => {
               window.localStorage.removeItem("profile");
@@ -161,9 +160,10 @@ export default function NavBar() {
     );
 
   if (!isLargeViewPort)
-    return (<>
-      <header
-        className="
+    return (
+      <>
+        <header
+          className="
     flex
     flex-row
     bg-black
@@ -176,50 +176,49 @@ export default function NavBar() {
     justify-between
     items-center
     "
-      >
-        <img
-          className="
+        >
+          <img
+            className="
         h-1/4
         mx-8
         cursor-pointer
       "
-          src="/LandingPage/NavBar-White.png"
-          alt="NavBar"
-          onClick={() => setIsNavOpen(!isNavOpen)}
-        />
-        <Link href="/">
-          <img
-            className="
+            src="/LandingPage/NavBar-White.png"
+            alt="NavBar"
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          />
+          <Link href="/">
+            <img
+              className="
           w-40
           cursor-pointer
         "
-            src="/LandingPage/Logo-White-Text.svg"
-            alt="TEDxDTU Logo"
-          />
-        </Link>
+              src="/LandingPage/Logo-White-Text.svg"
+              alt="TEDxDTU Logo"
+            />
+          </Link>
 
-        <Link href={form ? "/" : "/register"}>
-          <button
-            className="
+          <Link href={form ? "/" : "/register"}>
+            <button
+              className="
       bg-red-600
       mx-8
       h-16
       w-24
       md:w-32
       "
-            onClick={() => {
-              window.localStorage.removeItem("profile");
-              setForm(null);
-            }}
-          >
-            {form ? "Log Out" : "Register"}
-
-          </button>
-        </Link>
-      </header>
-      {isNavOpen && (
-        <nav
-          className="
+              onClick={() => {
+                window.localStorage.removeItem("profile");
+                setForm(null);
+              }}
+            >
+              {form ? "Log Out" : "Register"}
+            </button>
+          </Link>
+        </header>
+        {isNavOpen && (
+          <nav
+            className="
         z-20
         bg-black
         fixed
@@ -235,91 +234,94 @@ export default function NavBar() {
         items-center
         overflow-auto
       "
-        >
-          <Link href="/">
-            <button
-              className="
+          >
+            <Link href="/">
+              <button
+                className="
             bg-red-600
             rounded
             w-3/4
             py-4
             my-4
           "
-            >
-              Home
-            </button>
-          </Link>
+              >
+                Home
+              </button>
+            </Link>
 
-          <Link href="/theme">
-            <button
-              className="
+            <Link href="/theme">
+              <button
+                className="
             bg-red-600
             rounded
             w-3/4
             py-4
             my-4
           "
-            >
-              Theme
-            </button>
-          </Link>
+              >
+                Theme
+              </button>
+            </Link>
 
-          <Link href="/events">
-            <button
-              className="
+            <Link href="/events">
+              <button
+                className="
             bg-red-600
             rounded
             w-3/4
             py-4
             my-4
           "
-            >
-              Events
-            </button>
-          </Link>
+              >
+                Events
+              </button>
+            </Link>
 
-          {form && <Link href="/dashboard">
-            <button
-              className="
+            {form && (
+              <Link href="/dashboard">
+                <button
+                  className="
             bg-red-600
             rounded
             w-3/4
             py-4
             my-4
           "
-            >
-              Dashboard
-            </button>
-          </Link>}
+                >
+                  Dashboard
+                </button>
+              </Link>
+            )}
 
-          <Link href="/partners">
-            <button
-              className="
+            <Link href="/partners">
+              <button
+                className="
             bg-red-600
             rounded
             w-3/4
             py-4
             my-4
           "
-            >
-              Partners
-            </button>
-          </Link>
+              >
+                Partners
+              </button>
+            </Link>
 
-          <Link href="/about">
-            <button
-              className="
+            <Link href="/about">
+              <button
+                className="
             bg-red-600
             rounded
             w-3/4
             py-4
             my-4
           "
-            >
-              About
-            </button>
-          </Link>
-        </nav>
-      )}
-    </>);
+              >
+                About
+              </button>
+            </Link>
+          </nav>
+        )}
+      </>
+    );
 }

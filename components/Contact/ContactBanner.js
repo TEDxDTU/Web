ebb3eb76cbeffa5b49d1605c4e55eb61c9bfafea
Shivam311201 <gargshivam311201@gmail.com>
2022-10-05 20:44:31 +0530

    fixed contact us page

--- components/Contact/ContactBanner.js
+++ components/Contact/ContactBanner.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
 import { BsInstagram, BsTwitter, BsLinkedin } from "react-icons/bs";
-// import { IoCall } from "react-icons/io";
-// import { FiMail } from "react-icons/fi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ContactContent from "./ContactContent";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -14,18 +15,29 @@ const AboutBanner = () => {
   return (
     <div className="lg:grid lg:grid-cols-3 w-full py-16">
       <div className="col-span-2">
-        <div className="mx-auto mb-16 lg:mb-32 w-3/4 h-fit" data-aos="fade-up">
-          {/* <div className="icon"><IoCall /></div> */}
-          <p className="bg-[rgba(100,100,100,0.3)] rounded p-4 my-4 text-xl text-justify">
-            +91 99xxxxxxxx
+
+        <ContactContent
+          contents={[
+            [<FontAwesomeIcon icon={faPhone} />, "+91 99xxxxxxxx"],
+            [<FontAwesomeIcon icon={faEnvelope} />, "tedxdtu@dtu.ac.in"],
+          ]}
+        />
+        {/* <div className="mx-auto mb-16 lg:mb-32 w-3/4 " data-aos="fade-up">
+          <p className="bg-[rgba(100,100,100,0.3)] rounded p-4 my-4 text-xl text-justify flex">
+            <div className="h-4 w-4 mt-1.5">
+              <FontAwesomeIcon icon={faPhone} />
+            </div>
+            &nbsp;&nbsp;+91 99xxxxxxxx
           </p>
         </div>
         <div className="mx-auto mb-16 lg:mb-32 w-3/4 h-fit" data-aos="fade-up">
-          {/* <div className="icon"><FiMail /></div> */}
-          <p className="bg-[rgba(100,100,100,0.3)] rounded p-4 my-4 text-xl text-justify">
-            tedxdtu@mail
+          <p className="bg-[rgba(100,100,100,0.3)] rounded p-4 my-4 text-xl text-justify flex">
+            <div className="h-4 w-4 mt-1.5">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+            &nbsp;&nbsp;tedxdtu@mail
           </p>
-        </div>
+        </div> */}
         <div className="flex flex-row">
           <div className="ml-4">
             <a href="https://www.instagram.com/tedxdtu/" target="_blank">
--- components/Contact/ContactContent.js
+++ components/Contact/ContactContent.js
@@ -3,17 +3,20 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
-export default function ContactContent() {
+export default function ContactContent({ contents }) {
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
 
-  return (
-    <div className="mx-auto mb-16 lg:mb-32 w-3/4 h-fit" data-aos="fade-up">
-      <div className="icon">{content[0]}</div>
-      <p className="bg-[rgba(100,100,100,0.3)] rounded p-4 my-4 text-xl text-justify">
-        {content[1]}
+  return (<>
+    {contents?.map((content, val) => <div key={val} className="mx-auto mb-16 lg:mb-32 w-3/4 " data-aos="fade-up">
+      <p className="bg-[rgba(100,100,100,0.3)] rounded p-4 my-4 text-xl text-justify flex">
+        <div className="h-4 w-4 mt-1.5">
+          {content[0]}
+        </div>
+        &nbsp;&nbsp;{content[1]}
       </p>
-    </div>
+    </div>)}
+  </>
   );
 }

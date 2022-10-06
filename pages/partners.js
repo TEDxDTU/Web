f723a9c583015379f4f3e4bc759ea74709aa950b
Shivam311201 <gargshivam311201@gmail.com>
2022-10-06 13:55:09 +0530

    updated page title

--- pages/partners.js
+++ pages/partners.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Landing from '../components/Partners/Landing';
 import Page from '../components/Universal/Page';
 export default function Partner() {
-  return (<Page>
+  return (<Page pageTitle={"Partners"}>
     <header
       className='text-center text-6xl py-8 text-black text-stroke-thin-red font-bold '
     >Our Partners
